c103b5aeb50d35ff16532c0f8a613a3ab49a481b
GioDev <giosc430@gmail.com>
2026-04-30 01:39:29 -0400

    Implemented static schedule view in admin dashboard

--- eagle_eats_backend/admin.py
+++ eagle_eats_backend/admin.py
@@ -32,21 +32,29 @@ def get_schedule(kitchen):
     if schedule_id is None:
         return None
     
-    try:
-        schedule_name = database_ref.execute(
-            'select name from schedule where schedule.id=?',
-            (schedule_id,)
-        ).fetchone()
-        weekly_schedule = database_ref.execute(
-            'select day_of_week, isClosed, breakfast_open,breakfast_closed,lunch_open,lunch_closed,dinner_open,dinner_closed'
-            ' from kitchen_weekly_schedule where kitchen_weekly_schedule.schedule_id=?',
-            (schedule_id,)
-        ).fetchall()
-        return {
-            "name": schedule_name['name'],
-            "schedule": weekly_schedule
-        }
-    except:
-        error = "GET erorr on kitchen info level"
-        flash(error)
+
+    schedule_name = database_ref.execute(
+        'select name from schedule where schedule.id=?',
+        (schedule_id,)
+    ).fetchone()
+    weekly_schedule = database_ref.execute(
+        'select day_of_week, isClosed, breakfast_open,breakfast_closed,lunch_open,lunch_closed,dinner_open,dinner_closed'
+        ' from kitchen_weekly_schedule where kitchen_weekly_schedule.schedule_id=?',
+        (schedule_id,)
+    ).fetchall()
+    return {
+        "name": schedule_name['name'],
+        "week": organize_schedule(weekly_schedule)
+    }
+
+def organize_schedule(weekly_schedule):
+    if weekly_schedule is None:
         return None
+    
+    dayofweek_keys = ["1", "2", "3", "4", "5", "6", "7"]
+    org_schedule = dict.fromkeys(dayofweek_keys)
+
+    for weekday_schedule in weekly_schedule:
+        org_schedule[str(weekday_schedule["day_of_week"])] = weekday_schedule
+    
+    return org_schedule
